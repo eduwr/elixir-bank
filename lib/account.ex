4f5687c79d2d259b6314acae53f9acd60fa9bb7c
eduwr <eduwric@gmail.com>
2021-06-08 19:37:25 -0300

    feat: transfer method

--- lib/account.ex
+++ lib/account.ex
@@ -2,4 +2,22 @@ defmodule Account do
   defstruct user: User, balance: nil
 
   def register(user), do: %__MODULE__{user: user, balance: 1000}
+
+  def transfer(accounts, from, to, amount) do
+    from = Enum.find(accounts, fn account -> account.user.email == from.user.email end)
+
+    cond do
+      balance_validation(from.balance, amount) -> {:error, "Saldo insuficiente!"}
+
+      true ->
+        to = Enum.find(accounts, fn account -> account.user.email == to.user.email end)
+
+        from = %Account{from | balance: from.balance - amount}
+        to = %Account{to | balance: to.balance + amount}
+
+      [from, to]
+    end
+  end
+
+  defp balance_validation(balance, amount), do: balance < amount
 end
